e44ad0835292f23c05aea824ecf420d045612a9a
Tr8n <siddhantshukla3441@gmail.com>
2025-09-09 23:20:38 +0530

    cors error minor bug fixed

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,24 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('./connection/db');
 const userRoutes = require('./routes/userRoutes');
-const linkRoutes = require('./routes/linkRoutes');
+const linkRoutes = require('./routes/linkroutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // CORS configuration for local development
-app.use(cors({ 
-  origin: ["http://localhost:3000", "http://localhost:5173", "http://localhost:5174"],
-  credentials: true 
+// 
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://link-up-gules.vercel.app",
+    "https://link-up-git-main-tr8ns-projects.vercel.app"
+  ],
+  credentials: true
 }));
+
 app.use(bodyParser.json());
 
 app.use('/api/users', userRoutes);
