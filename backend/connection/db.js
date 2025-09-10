200671e50bcca77bfaeeae636d6550250c427b51
Tr8n <siddhantshukla3441@gmail.com>
2025-09-10 09:48:00 +0530

    db connection with string added

--- backend/connection/db.js
+++ backend/connection/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = "mongodb+srv://jjoker3438_db_user:PkpqR6hzerocD0my@cluster0.txovw1q.mongodb.net/LinkUp?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URI = "mongodb+srv://jjoker3438_db_user:PkpqR6hzerocD0my@cluster0.txovw1q.mongodb.net/LinkUp?retryWrites=true&w=majority&appName=Cluster0/link"
 
 if (!MONGO_URI) {
   console.error("❌ MONGO_URI is not set. Did you forget to add it in Render?");
   process.exit(1);
 }
-
+  
 mongoose.connect(MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch((err) => console.error('❌ MongoDB connection error:', err));
